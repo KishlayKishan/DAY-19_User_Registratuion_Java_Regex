a445bd11a0665329fae505a9a0b6832913624403
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-12 22:24:29 +0530

    Added:Check Regex For First Name logic

--- src/com/bridgelabz/javaregex/User_Registration_Main.java
+++ src/com/bridgelabz/javaregex/User_Registration_Main.java
@@ -1,10 +1,28 @@
+/*@Purpose:: As a User need to enter a valid First Name
+ *           First name starts with Cap and has minimum 3 characters
+ *@File:: User Registration Using Regex
+ *@Author:: Kishlay Kishan
+ */
+
 package com.bridgelabz.javaregex;
+
+import java.util.Scanner;
 
 public class User_Registration_Main {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration Program");
+	static Scanner scanner = new Scanner(System.in);
 
+	public static void welcome() {
+		System.out.println("Welcome to User Registation Program");
 	}
 
+	public static void main(String[] args) {
+		welcome();
+		System.out.println("Enter 1 : to validate First Name ");
+		switch (scanner.nextInt()) {
+		case 1:
+			User_Registration.validFirstName();
+			break;
+		}
+	}
 }
